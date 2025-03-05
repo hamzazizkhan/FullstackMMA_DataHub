44a967f0acc3549fcb7cb7889c9f65fc4b02350f
hamza <hamzazizkhan@gmail.com>
2025-03-05 17:40:29 +0300

    refactored to use pandas instead of .txt

--- backend/one_fighter.py
+++ backend/one_fighter.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import os
 from main_table import tables
 import sqlite3
+import pandas as pd
+import numpy as np
 
 # conn = sqlite3.connect('fighters.sqlite')
 # cur = conn.cursor()
@@ -126,6 +128,125 @@ def write_to_txt(diff, start_row, cur):
         fighters.close()
 
     print('program started?', start)
+
+def appendToSql(diff, start_row, cur, conn):
+    if diff==0:
+        start=False
+        return
+    else:
+        start=True
+
+    cur.execute('SELECT firstName, lastName, html FROM visited_links')
+    data = cur.fetchall()
+    if start:
+        for row_index in range(start_row,len(data)):
+            #print(row[0])
+            html = data[row_index][2]
+            fighter_name, prof_rec_table, main_rec_table = tables(html)
+        
+            prof_rec_data = {}
+            prof_rec_list = prof_rec_table.tbody.get_text().split()
+
+            first_name=fighter_name.split()[0]
+            last_name = fighter_name.split()[1]
+
+            prof_rec_data['firstName'] = [first_name]
+            prof_rec_data['lastName'] = [last_name]
+
+            # print(prof_rec_list)
+            for i in range(len(prof_rec_list)):
+                if i >17: break
+                if i%2==0 and i<6:
+                    prof_rec_data[prof_rec_list[i+1]] = [int(prof_rec_list[i])]
+                if prof_rec_list[i]=='By':
+                    prof_rec_data[prof_rec_list[i+1]+ 'Wins'] =  [int(prof_rec_list[i+2])]
+                    prof_rec_data[prof_rec_list[i+1]+ 'Losses'] =  [int(prof_rec_list[i+3])]
+            # print('\n')
+            # print(prof_rec_data)
+
+            #print(prof_rec_data)
+            df_prof = pd.DataFrame(prof_rec_data)
+            #print(df_prof)
+
+            # Export the DataFrame to the SQLite database
+            table_name = 'profesional_record_data'
+            df_prof.to_sql(table_name, conn, if_exists='append', index=False)
+
+            ################################ main table
+
+            MMA_record = {'firstName':[], 'lastName':[], 'result':[], 'record':[], 'opponent':[], 'result':[], 'method':[], 'event':[], 'date':[], 
+              'round':[], 'time':[], 'location':[], 'notes':[]}
+
+            rows = main_rec_table.find_all('tr')
+            qu = []
+            i=0
+            rowspan = []
+            inserts=0
+            texts=[]
+            for row in rows:
+                if i ==0:
+                    i=1
+                    continue
+                #print(row)
+                td = row.find_all('td')
+                text = [td_tag.text.strip() for td_tag in td]
+
+
+                if len(rowspan)!=0:
+                    for ele in rowspan:
+                        text_insert = ele[0]
+                        index_insert = ele[1]
+                        
+                        text.insert(index_insert, text_insert)
+                    if inserts==1:
+                        rowspan=[]
+                        inserts=0
+                    else:
+                        inserts-=1
+
+                else:
+                    #rowspan = [(int(td_tag['rowspan'])-1, td_tag.text.strip(), i) for i, td_tag in enumerate(td) if 'rowspan' in td_tag.attrs]
+                    for i, td_tag in enumerate(td):
+                        if 'rowspan' in td_tag.attrs:
+                            rowspan.append((td_tag.text.strip(), i))
+                            inserts = int(td_tag['rowspan'])-1
+                if len(text)<10:
+                    diff = 10-len(text)
+                    for _ in range(diff):
+                        text.append('Null')
+
+                texts.append(text)
+
+            #print(texts)
+
+            for val in range(len(texts)):
+                MMA_record['firstName'].append(first_name)
+                MMA_record['lastName'].append(last_name)
+
+            # print('\n')
+            # print(MMA_record)
+
+            # print('\n===')
+            texts = np.array(texts)
+
+            MMA_record['result'] = list(map(lambda x:x[0], texts[:,0:1]))
+            MMA_record['record'] = list(map(lambda x:x[0], texts[:,1:2]))
+            MMA_record['opponent'] = list(map(lambda x:x[0], texts[:,2:3]))
+            MMA_record['method'] = list(map(lambda x:x[0], texts[:,3:4]))
+            MMA_record['event'] = list(map(lambda x:x[0], texts[:,4:5]))
+            MMA_record['date'] = list(map(lambda x:x[0], texts[:,5:6]))
+            MMA_record['round'] = list(map(lambda x:x[0], texts[:,6:7]))
+            MMA_record['time'] = list(map(lambda x:x[0], texts[:,7:8]))
+            MMA_record['location'] = list(map(lambda x:x[0], texts[:,8:9]))
+            MMA_record['notes'] = list(map(lambda x:x[0], texts[:,9:10]))
+
+            df_MMArecord = pd.DataFrame(MMA_record)
+
+            # # Export the DataFrame to the SQLite database
+            table_name = 'MMA_record'
+            df_MMArecord.to_sql(table_name, conn, if_exists='append', index=False)
+
+
 
 # if __name__=="__one_fighter__":
 #     pass
--- backend/populate.py
+++ backend/populate.py
@@ -4,7 +4,7 @@ import os
 from main_table import tables
 import sqlite3
 
-from one_fighter import check_tables_update, write_to_txt
+from one_fighter import check_tables_update, write_to_txt, appendToSql
 from fighterDB import add_to_main_table, add_to_prof_rec
 
 conn = sqlite3.connect('../data/fighters.sqlite')
@@ -12,14 +12,26 @@ cur = conn.cursor()
 
 diff, start_row = check_tables_update(cur)
 # alrdy checks for diff - write_to_txt
-write_to_txt(diff,start_row, cur)
 
-if diff!=0:
-    add_to_prof_rec(cur)
-    conn.commit()
-    add_to_main_table(cur)
-    conn.commit()
+textVersion = False
+
+if textVersion:
+    write_to_txt(diff,start_row, cur)
+
+    if diff!=0:
+        add_to_prof_rec(cur)
+        conn.commit()
+        add_to_main_table(cur)
+        conn.commit()
+    else:
+        print('prof_record_data and MMA_record tables up to date!')
+
+    cur.close()
 else:
-    print('prof_record_data and MMA_record tables up to date!')
-
-cur.close()
+    if diff!=0:
+        appendToSql(diff,start_row, cur, conn)
+        conn.commit()
+        cur.close()
+    else:
+        print('prof_record_data and MMA_record tables up to date!')
+    
