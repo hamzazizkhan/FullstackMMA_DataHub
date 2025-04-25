fdedefdd6e6b6b767e04005e98f55fdf0f503817
hamza <hamzazizkhan@gmail.com>
2025-04-25 16:39:30 +0300

    editing passenger_Wsgi

--- server_API/passenger_wsgi.py
+++ server_API/passenger_wsgi.py
@@ -1,9 +1,1 @@
-import imp
-import os
-import sys
-
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-wsgi = imp.load_source('wsgi', 'main.py')
-application = wsgi.app
+from main import app as application
