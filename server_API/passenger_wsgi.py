729a423638453e61384659739332d60249c919ce
hamza <hamzazizkhan@gmail.com>
2025-04-25 19:01:20 +0300

    added path to passegner

--- server_API/passenger_wsgi.py
+++ server_API/passenger_wsgi.py
@@ -1,1 +1,4 @@
-from main import app as application
+import sys, os
+sys.path.insert(0, os.path.dirname(__file__))
+
+from main import app as application  # change 'main' to the name of your Flask file if different
